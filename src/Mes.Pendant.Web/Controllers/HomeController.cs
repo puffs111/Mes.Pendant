d3b812a215c95fd542b0e9e7106f4d58d8768aa5
小羊饼饼 <2719323699@qq.com>
2025-01-07 10:20:16 +0800

    11111

--- src/Mes.Pendant.Web/Controllers/HomeController.cs
+++ src/Mes.Pendant.Web/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ namespace Mes.Pendant.Web.Controllers
     public class HomeController : Controller
     {
         /// <summary>
-        /// 可恶的洋洋11
+        /// 可恶的洋洋1111
         /// </summary>
         /// <returns></returns>
         public IActionResult Index()
